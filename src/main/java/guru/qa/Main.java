cb2efead55df9c9ac003177b85d767ede740ee99
EphimSh <ephimizm@gmail.com>
2023-05-31 00:05:39 +0900

    message added

--- src/main/java/guru/qa/Main.java
+++ src/main/java/guru/qa/Main.java
@@ -3,6 +3,6 @@ package guru.qa;
 public class Main {
     public static void main(String[] args) {
         System.out.println("goodbye world1");
-        System.out.println("really");
+        System.out.println("ok");
     }
 }
